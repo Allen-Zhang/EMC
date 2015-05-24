f6fa15125c3384f33274fa93564ab4c1a10c0aa8
Allen Yingyuan Zhang <zyy2013@bu.edu>
2015-05-23 21:12:09 -0400

    Refactor the Framework

--- src/com/mercury/dao/PersonDao.java
+++
@@ -1,7 +0,0 @@
-package com.mercury.dao;
-
-import com.mercury.bean.Person;
-
-public interface PersonDao {
-	public Person findPersonByEmail(String email);
-}
--- src/com/mercury/service/CustomUserDetailsService.java
+++ src/com/mercury/service/CustomUserDetailsService.java
@@ -17,41 +17,7 @@ import com.mercury.dao.UserDao;
 
 @Service
 @Transactional(readOnly=true)
-public class CustomUserDetailsService implements UserDetailsService{
-//	private PersonDao pd;
-//		
-//	public PersonDao getPd() {
-//		return pd;
-//	}
-//	public void setPd(PersonDao pd) {
-//		this.pd = pd;
-//	}
-//	
-//	@Override
-//	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//		UserDetails user = null;  
-//		try {
-//			Person person = pd.findPersonByEmail(email);	
-//			Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-//			for (String authority : person.getAuthorities()) {
-//				authorities.add(new SimpleGrantedAuthority(authority));
-//			}
-//			user = new User(
-//						person.getUsername(),
-//						person.getPassword(),
-//						true,
-//						true,
-//						true,
-//						true,
-//						authorities 
-//					);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			throw new UsernameNotFoundException("Error in retrieving user");
-//		}
-//		return user;
-//	}
-	
+public class CustomUserDetailsService implements UserDetailsService{	
 	private UserDao ud;
 
 	public UserDao getUd() {
