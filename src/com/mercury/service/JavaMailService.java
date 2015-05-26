6e9e8270e46b3e98fb5abc4d9fdf243b4502d7ba
sijiyy <sijiyangyi24@gmail.com>
2015-05-26 15:04:20 -0400

    JavaMail Service

---
+++ src/com/mercury/service/JavaMailService.java
@@ -0,0 +1,12 @@
+package com.mercury.service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+public class JavaMailService {
+
+}
