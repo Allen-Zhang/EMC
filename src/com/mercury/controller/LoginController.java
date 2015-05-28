19b80e5509a7ac59193236451fad56863464469b
sijiyy <sijiyangyi24@gmail.com>
2015-05-28 13:07:37 -0400

    delete email validate
    delete email validate

--- src/com/mercury/controller/LoginController.java
+++ src/com/mercury/controller/LoginController.java
@@ -6,16 +6,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import com.mercury.service.JavaMailService;
-import com.mercury.service.EmailValidateService;
-
 @Controller
 public class LoginController {
-	@Autowired
-	@Qualifier("javaMailService")
-	private JavaMailService jms;
+
 	
 	@RequestMapping(value="/security/login", method = RequestMethod.GET)
 	public String login(ModelMap model) {
@@ -25,21 +18,8 @@ public class LoginController {
 	@RequestMapping(value="/main", method = RequestMethod.GET)
 	public ModelAndView mainPage() {	
 		ModelAndView mav = new ModelAndView();
-		
-		String from = "sijiyangyi24@gmail.com";
-		String to = "iloveliverpoolgo@gmail.com";
-		String subject = "Thanks for registeration!";
-		String msg = "Dear new user:Please click the following link to activate your account";
-		EmailValidateService evs = new EmailValidateService();
-		if (evs.validateEmail(to)) {
-			jms.sendMail(from, to, subject, msg);
-			mav.setViewName("hello");
-			mav.addObject("title", "Hello, welcome to Customized Spring Security");
-		}else{
-		mav.setViewName("login");
-		mav.addObject("message", "Fail");
-		}
-
+		mav.setViewName("hello");
+		mav.addObject("title", "Hello, welcome to Customized Spring Security");
 		return mav;
 	}
 }
