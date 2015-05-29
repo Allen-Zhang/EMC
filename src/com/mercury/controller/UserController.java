0e94fca6325ddf398a20aba63df7d8ec6e9dc825
sijiyy <sijiyangyi24@gmail.com>
2015-05-28 23:33:11 -0400

    UserController and UserService
    UC and US

--- src/com/mercury/controller/UserController.java
+++ src/com/mercury/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.mercury.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +10,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mercury.bean.User;
-import com.mercury.dao.impl.UserDaoImpl;
+import com.mercury.service.UserService;;
 
 @Controller
 public class UserController {
+	@Autowired
+	@Qualifier("UserService")
+	private UserService us;
 	@RequestMapping(value="/account/signup", method = RequestMethod.GET)
 	public String accountPage(){
 		return "/account/signup";
 	}
 	@RequestMapping(value = "/account/register", method = RequestMethod.GET)	
 	@ResponseBody
-	public ModelAndView checkUser(@RequestBody User user)   {
-		User user1 = new User();
+	public ModelAndView processUser(@RequestBody User user)   {
+/*		User user1 = new User();
 		user1.setUsername("freddie");
 		user1.setPassword("freddie");
 		UserDaoImpl udi = new UserDaoImpl();
@@ -31,6 +36,22 @@ public class UserController {
 		} else{
 			udi.save(user1);
 			mav.setViewName("");
+			mav.addObject("title", "Hello, the username is OK");
+			return mav;
+		}*/
+/*		String username = user.getUsername();
+		String passwaor = user.getPassword();*/
+		User user1 = new User();
+		user1.setUsername("allen");
+		user1.setPassword("allen");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("Error");
+		mav.addObject("title", "Hello, the username is existed");
+		if(us.checkUser(user1.getUsername())!= null){
+			return mav;
+		}else{
+			us.saveUser(user1);
+			mav.setViewName("Thanks for your registeration");
 			mav.addObject("title", "Hello, the username is OK");
 			return mav;
 		}
--- src/com/mercury/service/UserService.java
+++ src/com/mercury/service/UserService.java
@@ -1,5 +1,25 @@
 package com.mercury.service;
 
-public class UserService {
+import com.mercury.bean.User;
+import com.mercury.dao.UserDao;
 
+public class UserService {
+	private UserDao ud;
+
+	
+
+	public UserDao getUd() {
+		return ud;
+	}
+	public void setUd(UserDao ud) {
+		this.ud = ud;
+	}
+	public User checkUser(String username){
+		User user = ud.findByUserName(username);
+		return user;
+	}
+	public void saveUser(User user){
+		ud.save(user);
+	}
+	
 }
