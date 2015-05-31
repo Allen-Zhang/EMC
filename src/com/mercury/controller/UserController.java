7bdf795b37ad21ecc8322efd26dbc2d423d17ecd
sijiyy <sijiyangyi24@gmail.com>
2015-05-30 20:57:52 -0400

    UserController UserService activate.jsp
    ...

--- src/com/mercury/controller/UserController.java
+++ src/com/mercury/controller/UserController.java
@@ -4,15 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+//import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mercury.bean.User;
+import com.mercury.service.JavaMailService;
 import com.mercury.service.UserService;;
 
 @Controller
@@ -21,6 +23,11 @@ public class UserController {
 	@Autowired
 	@Qualifier("userService")
 	private UserService us;
+	
+	@Autowired
+	@Qualifier("javaMailService")
+	private JavaMailService jms;
+		
 	@RequestMapping(value="/account/signup", method = RequestMethod.GET)
 	public String accountPage(){
 		return "/account/signup";
@@ -28,47 +35,6 @@ public class UserController {
 	
 	@RequestMapping(value = "/account/register", method = RequestMethod.POST)	
 	@ResponseBody
-	/*public ModelAndView processUser(HttpServletRequest request)   {
-		User user1 = new User();
-		user1.setUsername("freddie");
-		user1.setPassword("freddie");
-		UserDaoImpl udi = new UserDaoImpl();
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("Error");
-		mav.addObject("title", "Hello, the username is existed");
-		if(udi.findByUserName(user1.getUsername())!= null){
-			return mav;
-		} else{
-			udi.save(user1);
-			mav.setViewName("");
-			mav.addObject("title", "Hello, the username is OK");
-			return mav;
-		}
-		User user1 = new User();
-		user1.setUsername("allen");
-		user1.setPassword("allen");
-		System.out.println("abc: "+user.getUsername());
-		System.out.println("bb: "+user.getPassword());
-		System.out.println("cc: "+user.getEmail());
-		User user = new User();
-		user.setUsername(request.getParameter("username"));
-		user.setPassword(request.getParameter("password"));
-		user.setEmail(request.getParameter("email"));
-		ModelAndView mav = new ModelAndView();
-		// Duplicates
-		if(us.checkUser(user.getUsername())!= null){
-			mav.setViewName("signup");
-			mav.addObject("message", "Hello, the username is existed");
-			return mav;
-		// Success
-		}else{
-			us.saveUser(user);
-			// send email here....
-			mav.setViewName("home");
-			mav.addObject("message", "Hello, the username is OK");
-			return mav;
-		}
-	}*/
 	public ModelAndView processUser(@ModelAttribute("user") User user)   {
 				ModelAndView mav = new ModelAndView();
 				// Duplicates
@@ -80,6 +46,12 @@ public class UserController {
 				}else{
 					us.saveUser(user);
 					// send email here....
+					String from = "sijiyangyi24@gmail.com";
+					String to = user.getEmail();
+					String subject = "Dear: " + user.getUsername();
+					String msg = "Please click the following link to activate your account" +"\n" + 
+					"http://localhost:8080/EMC/account/activate.html?id="+user.getUsername();
+					jms.sendMail(from, to, subject, msg);
 					mav.setViewName("home");
 					mav.addObject("message", "Hello, the username is OK");
 					return mav;
@@ -106,4 +78,12 @@ public class UserController {
 		return mav;
 		
 	}
+	//activate user account
+	@RequestMapping(value="/account/activate", method = RequestMethod.GET)
+	public String activateUserAccountPage(@RequestParam("id") String id){
+		System.out.println(id);
+		us.activateUser(id);
+		return "/account/activate";
+	}
+
 }
