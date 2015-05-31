4b17992d22a38b6a52bd0c9b16a59052133442dc
sijiyy <sijiyangyi24@gmail.com>
2015-05-31 14:32:48 -0400

    admin showInterest updateInterestRate
    ...

---
+++ WebContent/js/controller/adminController.js
@@ -0,0 +1,31 @@
+/* 
+ * Mortgage Calculator Controller
+ * @Framework: AngularJS 
+ */
+angular.module("myApp")
+	.controller("adminCtrl", ['$scope','$http',function($scope,$http){
+		// Making the fields empty
+		$scope.username='';
+		$scope.password='';
+		$scope.email='';
+
+		$scope.user = [];
+		$scope.register = function(){	
+			$scope.user.push({'username' : $scope.username, 'password' : $scope.password, 
+				'email' : $scope.email});
+			// Writing it to the server
+			var dataObj = {
+					username : $scope.username,
+					password : $scope.password,
+					email : $scope.email
+			};	
+			$http.post('register.html', dataObj)
+			.success(function(data, status, headers, config) {
+				$scope.userInfo = data;
+			})
+			.error(function(data, status, headers, config) {
+				alert( "failure message: " + JSON.stringify({data : data}));  // JSON.stringify() converts a JavaScript value to a JSON string
+			});		
+		
+		};
+	}]);
